31cfa55ccdb2cd4a852d04c4fcc94a930f709d60
Anhelina Zhurauliova <anhelina-zhurauliova>
2020-03-17 22:54:09 +0300

    feat: resolve task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let config = [];
+  for (let i = 0; i < bracketsConfig.length; i++) {
+    config.push(bracketsConfig[i].join(''))
+  }
+  for (let j = 0; j < config.length; j++) {
+    console.log(j)
+    if (str.includes(config[j])) {
+      console.log('here')
+      str = str.replace(config[j], '')
+      j = -1;
+    }
+  }
+  if (str.length == 0) {
+    return true;
+  }
+  else {
+    return false;
+  }
 }
